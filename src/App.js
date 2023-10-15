692c5fe172bfb98a2ccec7dab9ac11bb2f26f6bf
Fabbeiru <fabian@macbook.local>
2023-10-15 23:11:59 +0200

    Prototype styled

--- src/App.js
+++ src/App.js
@@ -1,143 +1,141 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import './App.css';
 import "@aws-amplify/ui-react/styles.css";
-import { API, Storage } from 'aws-amplify';
 import {
-  Button,
-  Flex,
+  Authenticator,
+  useTheme,
   Heading,
-  Image,
   Text,
-  TextField,
+  Tabs,
+  TabItem,
+  Button,
   View,
-  withAuthenticator,
-} from '@aws-amplify/ui-react';
-import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
+  Image
+} from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
 
-const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
+Amplify.configure(awsExports);
 
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await Storage.get(note.name);
-          note.image = url;
-        }
-        return note;
-      })
-    );
-    setNotes(notesFromAPI);
-  }
-
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const image = form.get("image");
-    const data = {
-      name: form.get("name"),
-      description: form.get("description"),
-      image: image.name,
-    };
-    if (!!data.image) await Storage.put(data.name, image);
-    await API.graphql({
-      query: createNoteMutation,
-      variables: { input: data },
-    });
-    fetchNotes();
-    event.target.reset();
-  }
-  
-
-  async function deleteNote({ id, name }) {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-    await Storage.remove(name);
-    await API.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id } },
-    });
-  }
-
-  return (
-    <View className="App">
-    <br></br>
-    <br></br>
-    <br></br>
-      <iframe title="Report Section" width="800" height="400" src="https://app.powerbi.com/view?r=eyJrIjoiNmU0NmVlNmMtZGMwZi00MjE3LWFjNTItY2EzMDJlN2ZjMDlkIiwidCI6ImIyYmI3MzFjLTQ2MGQtNDIwZi1hNDc1LTNlZDYxNWE4Mjk4NyIsImMiOjh9" frameborder="0" allowFullScreen="true"></iframe>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <View
-            name="image"
-            as="input"
-            type="file"
-            style={{ alignSelf: "end" }}
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-      {notes.map((note) => (
-        <Flex
-          key={note.id || note.name}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Text as="strong" fontWeight={700}>
-            {note.name}
-          </Text>
-          <Text as="span">{note.description}</Text>
-          {note.image && (
-            <Image
-              src={note.image}
-              alt={`visual aid for ${notes.name}`}
-              style={{ width: 400 }}
-            />
-          )}
-          <Button variation="link" onClick={() => deleteNote(note)}>
-            Delete note
-          </Button>
-        </Flex>
-      ))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  );
+const formFields = {
+  confirmVerifyUser: {
+    confirmation_code: {
+      label: 'New Label',
+      placeholder: 'Enter your Confirmation Code:',
+      isRequired: false,
+    },
+  },
 };
 
-export default withAuthenticator(App);
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="DORS logo"
+          src={require("./DORS.png")}
+        />
+      </View>
+    );
+  },
+
+  VerifyUser: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+
+  ConfirmVerifyUser: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+};
+
+export default function App() {
+  return (
+      <>
+        <div className='container'>
+          <Authenticator
+            formFields={formFields}
+            components={components}
+            hideSignUp={true}
+          >
+            {({ signOut, user }) => (
+              <>
+                <div className='header'>
+                  <Image
+                    alt="DORS mini logo"
+                    src={require("./DORSMini.png")}
+                  />
+                  <div className='user-info'>
+                    <p>{user.username}</p>
+                    <Button variation="primary" size='small' onClick={signOut}>Sign out</Button>
+                  </div>
+                </div>
+                <main>
+                  <section>
+                    <h2>Reports</h2>
+                    <Tabs
+                      justifyContent="flex-start">
+                      <TabItem title="Project 1">
+                        <br></br>
+                        <iframe title="Report Section" width="800" height="400" src="https://app.powerbi.com/view?r=eyJrIjoiNmU0NmVlNmMtZGMwZi00MjE3LWFjNTItY2EzMDJlN2ZjMDlkIiwidCI6ImIyYmI3MzFjLTQ2MGQtNDIwZi1hNDc1LTNlZDYxNWE4Mjk4NyIsImMiOjh9" frameborder="0" allowFullScreen="true"></iframe>
+                      </TabItem>
+                      <TabItem title="Project 2">
+                        <br></br>
+                        <iframe title="Report Section" width="800" height="400" src="https://app.powerbi.com/view?r=eyJrIjoiNmU0NmVlNmMtZGMwZi00MjE3LWFjNTItY2EzMDJlN2ZjMDlkIiwidCI6ImIyYmI3MzFjLTQ2MGQtNDIwZi1hNDc1LTNlZDYxNWE4Mjk4NyIsImMiOjh9" frameborder="0" allowFullScreen="true"></iframe>
+                      </TabItem>
+                    </Tabs>
+                  </section>
+                  <section className='under-dev'>
+                    <div>
+                      <h2>Web Camera</h2>
+                      <Image
+                        className='under-icon'
+                        alt="Under construction icon"
+                        src={require("./pickaxe.png")}
+                      />
+                      <p>Currently under construction.</p>
+                    </div>
+                    <div>
+                      <h2>Telemetry Access</h2>
+                      <Image
+                        className='under-icon'
+                        alt="Under construction icon"
+                        src={require("./pickaxe.png")}
+                      />
+                      <p>Currently under construction.</p>
+                    </div>
+                  </section>
+                </main>
+              </>
+            )}
+          </Authenticator>
+        </div>
+      </>
+  );
+}
