7aadd049e364990d0b2281bf04ee5d94a9ce3f6f
Evnazx <emiliovinas01@gmail.com>
2024-07-06 13:21:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ export default function App() {
                       {user && user.username === 'test_user' && (
                         <TabItem title="AutReportTEST">
                           <br></br>
-                          <iframe title="" width="800" height="400" src="https://mybinder.org/v2/gh/Evnazx/AutoReportDORSTest/HEAD?labpath=ReportTest.ipynb" frameborder="0" allowFullScreen="true"></iframe>
+                          <iframe title="" width="800" height="400" src="https://mybinder.org/v2/gh/Evnazx/AutoReportDORSTest/HEAD?urlpath=lab/tree/ReportTest.ipynb" frameborder="0" allowFullScreen="true"></iframe>
                         </TabItem>
                       )}
                     </Tabs>
