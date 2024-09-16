c1093bb7905c185cf63955b34a45134106b6585b
Evnazx <emiliovinas01@gmail.com>
2024-09-16 15:22:56 +0200

    Update link form

--- src/App.js
+++ src/App.js
@@ -136,7 +136,7 @@ export default function App() {
                     </Tabs>
                   </section>
                   <section className='under-dev'>
-                    <a href='https://forms.office.com/e/Y7uXwVe1yS' target='_blank' rel="noreferrer">
+                    <a href='https://soiltechs-my.sharepoint.com/:x:/g/personal/tecnico_soiltechs_es/Ee-QJvFYkYtLsFnngFPL9p4BwH98bjAQjniAwYkilNxMUQ?e=a0yubt' target='_blank' rel="noreferrer">
                       <h2>
                         Informe de mejoras o errores
                         <svg xmlns="http://www.w3.org/2000/svg" fill='currentColor' height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg>
